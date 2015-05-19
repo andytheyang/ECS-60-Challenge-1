9875115d5fd0299ee9e7023bdc3ab0d9686a1692
andytheyang <achyang@ucdavis.edu>
2015-05-19 14:20:26 -0700

    stuff

--- HuffmanTree.cpp
+++ HuffmanTree.cpp
@@ -1,1 +1,3 @@
 #include "HuffmanTree.h"
+
+
--- HuffmanTree.h
+++ HuffmanTree.h
@@ -1,7 +1,18 @@
-// Author: Andy Yang
-#ifndef HUFFMANTREE_H
-#define HUFFMANTREE_H
+#ifndef TREE_H
+#define TREE_H
 
+class HuffmanTree
+{
+private:
+public:
+  HuffmanTree(const unsigned char *message, const int size);
+};
+
+
+class HuffmanNode
+{
+
+};
 
 
 #endif
